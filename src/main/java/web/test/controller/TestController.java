7af9f0fb4eef829722b339c8c1af0d49de5ecabe
EthanLong-dev <ethanlong41152@gmail.com>
2024-11-02 09:41:44 +0800

    TIA202專題順利！

---
+++ src/main/java/web/test/controller/TestController.java
@@ -0,0 +1,24 @@
+package web.test.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestController
+ */
+@WebServlet("/TestController")
+public class TestController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("TIA202專題順利！");
+	}
+	
+}
